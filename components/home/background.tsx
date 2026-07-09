013965b555098bc21fa5ea7749a61fb4e10d66f8
Bida3443 <alexanderbamise@gmail.com>
2026-07-09 14:28:44 -0700

    afrikabal

---
+++ components/home/background.tsx
@@ -0,0 +1,11 @@
+export default function BackgroundGlow() {
+    return (
+        <>
+            <div className="absolute -left-62.5 -top-30 h-162.5 w-162.5 rounded-full bg-[#E9F86A]/60 blur-[180px]" />
+
+            <div className="absolute left-45 -bottom-62.5 h-125 w-125 rounded-full bg-[#F6C36A]/40 blur-[180px]" />
+
+            <div className="absolute -right-55 top-45 h-162.5 w-162.5 rounded-full bg-[#9CE48D]/85 blur-[200px]" />
+        </>
+    );
+}
---
+++ components/home/hero.tsx
@@ -0,0 +1,13 @@
+import BackgroundGlow from "./background";
+
+export default function hero() {
+  return (
+<section className="relative overflow-hidden h-screen bg-white">
+    <BackgroundGlow />
+
+    <div className="relative z-10">
+        {/* Hero */}
+        <div>dddddddddddddddddddd</div>
+    </div>
+</section>
+)}
