0d0e632130c4499ac0cd8ce8158bd464610ee123
Bida3443 <alexanderbamise@gmail.com>
2026-07-12 20:43:41 -0700

    afrikabal

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -5,10 +5,9 @@ export default function hero() {
 <section className="relative overflow-hidden h-screen bg-white">
     <BackgroundGlow />
 
-    <div className="relative z-10 flex justify-center items-center py-10">
-    
-        <div className="flex flex-col">
-            <h1>Revolutionizing the Future of Agriculture and Finance in Africa with AfriKabal</h1>
+    <div className="relative z-10  py-10">
+        <div className="flex flex-col justify-center place-items-center items-center">
+            <h1 className="font-bold text-2xl">Revolutionizing the Future of Agriculture <br /> and Finance in Africa with AfriKabal</h1>
             <p>Discover ultimate convenience with Afrikabal: your all-in-one solution for financial and agricultural needs. Send money, explore fresh farm produce, and support local communities seamlessly.</p>
 
         </div>
