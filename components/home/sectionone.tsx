8a9cd3fcd446bed79edb26ffef1f32abc1c7e5dc
Bida3443 <alexanderbamise@gmail.com>
2026-07-21 14:58:32 -0700

    afrikabal

--- components/home/sectionone.tsx
+++ components/home/sectionone.tsx
@@ -12,11 +12,13 @@ export default  function sectionone  () {
             <h1 className='font-semibold  text-3xl'>Solving African Agriculture & <br /> Finance Challenges</h1>
           </div>
           <div className='flex mt-30 gap-50 justify-center'>
-            <div>
+            <div className='space-y-5'>
               <h1 className='text-3xl font-medium'>Finance</h1>
               <h1 className='text-3xl font-medium' >Agriculture</h1>
-              <p>Send, receive and request money <br /> and crypto on Afrikabal app</p>
-              <p className='lg:max-w-160 max-w-80.5'>Easily send, receive, and request money and cryptocurrency with the Afrikabal app. Seamlessly transfer funds and manage your finances with just a few taps. Join the digital revolution and experience secure, fast, and convenient transactions on Afrikabal.</p>
+              <div>
+                <p className='font-semibold pb-2'>Send, receive and request money <br /> and crypto on Afrikabal app</p>
+              <p className='lg:max-w-155 max-w-80.5'>Easily send, receive, and request money and cryptocurrency with the Afrikabal app. Seamlessly transfer funds and manage your finances with just a few taps. Join the digital revolution and experience secure, fast, and convenient transactions on Afrikabal.</p>
+              </div>
             </div>
             <div>ffffff</div>
           </div>
