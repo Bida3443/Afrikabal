3cf61855ecf30b555e7c756151789ab5a8eb3271
Bida3443 <alexanderbamise@gmail.com>
2026-07-21 15:13:17 -0700

    afrikabal

--- components/home/sectionone.tsx
+++ components/home/sectionone.tsx
@@ -20,6 +20,22 @@ export default  function sectionone  () {
                 <p className='font-semibold text-2xl pb-2'>Send, receive and request money <br /> and crypto on Afrikabal app</p>
               <p className='lg:max-w-155 max-w-80.5'>Easily send, receive, and request money and cryptocurrency with the Afrikabal app. Seamlessly transfer funds and manage your finances with just a few taps. Join the digital revolution and experience secure, fast, and convenient transactions on Afrikabal.</p>
               </div>
+
+        <a
+            href="/products"
+            className="group flex items-center gap-2 hover:text-black transition-all duration-300 cursor-pointer text-[#A5CD39]"
+          >
+            <p className="group-hover:underline">Download Afrikabal</p>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              className="w-3 h-3 fill-current transform transition-transform duration-300 group-hover:translate-x-1"
+            >
+              <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-105.4 105.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+            </svg>
+          </a>
+
             </div>
             <div><Image src={phonne} alt='' width={500} height={700}/></div>
           </div>
