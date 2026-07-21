096b5d075357d0407d54acb0e6eb534bebeaf151
Bida3443 <alexanderbamise@gmail.com>
2026-07-21 15:07:44 -0700

    afrikabal

--- components/home/sectionone.tsx
+++ components/home/sectionone.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import slash from "../../public/slash.png"
+import phonne from "../../public/phonne.png"
 
 export default  function sectionone  () {
   return (
@@ -11,16 +12,16 @@ export default  function sectionone  () {
             <Image src={slash} alt='' width={200} height={200}/>
             <h1 className='font-semibold  text-3xl'>Solving African Agriculture & <br /> Finance Challenges</h1>
           </div>
-          <div className='flex mt-30 gap-50 justify-center'>
+          <div className='flex mt-30 gap-20 justify-center'>
             <div className='space-y-5'>
-              <h1 className='text-3xl font-medium'>Finance</h1>
-              <h1 className='text-3xl font-medium' >Agriculture</h1>
+              <h1 className='text-4xl font-medium text-[#A5CD39]'>Finance</h1>
+              <h1 className='text-4xl font-medium text-[#C6C6C6]'>Agriculture</h1>
               <div>
-                <p className='font-semibold pb-2'>Send, receive and request money <br /> and crypto on Afrikabal app</p>
+                <p className='font-semibold text-2xl pb-2'>Send, receive and request money <br /> and crypto on Afrikabal app</p>
               <p className='lg:max-w-155 max-w-80.5'>Easily send, receive, and request money and cryptocurrency with the Afrikabal app. Seamlessly transfer funds and manage your finances with just a few taps. Join the digital revolution and experience secure, fast, and convenient transactions on Afrikabal.</p>
               </div>
             </div>
-            <div>ffffff</div>
+            <div><Image src={phonne} alt='' width={500} height={700}/></div>
           </div>
         </div>
     </section>
