5361d6bdc3c538b6dfc9733dac759e1a1493f815
Bida3443 <alexanderbamise@gmail.com>
2026-07-24 16:53:56 -0700

    afrikabal

--- components/home/sectiontwo.tsx
+++ components/home/sectiontwo.tsx
@@ -11,9 +11,9 @@ export default function  sectiontwo  () {
 
                 <div className='flex flex-col py-10 justify-center items-center'>
           <div className='flex flex-col text-center space-y-3 justify-center items-center'>
-            <p>Why Us</p>
+            <p>Our Features</p>
             <Image src={slash} alt='' width={200} height={200}/>
-            <h1 className='font-semibold  text-3xl'>Solving African Agriculture & <br /> Finance Challenges</h1>
+            <h1 className='font-semibold  text-3xl'>Feel the best experience with <br /> our features</h1>
           </div>
           <div className='flex mt-30 gap-20 justify-center'>
             <div className='space-y-5'>
