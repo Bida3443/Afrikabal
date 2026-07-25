90b09e9db101a9c4125c2c356768ba8d811e4173
Bida3443 <alexanderbamise@gmail.com>
2026-07-24 17:30:47 -0700

    afrikabal

--- components/home/sectiontwo.tsx
+++ components/home/sectiontwo.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import BackgroundGlow from './background'
 import Image from 'next/image'
 import slash from "../../public/slash.png"
-import phonne from "../../public/phonne.png"
+import Invest1 from "../../public/Invest 1.png"
+import Invest2  from "../../public/Invest 2.png"
+import Invest3 from "../../public/Invest 3.png"
+import Invest4 from "../../public/Invest 4.png"
+import Invest5 from "../../public/Invest 5.png"
+import Invest6 from "../../public/Invest 6.png"
 
 export default function  sectiontwo  () {
   return (
@@ -15,10 +20,11 @@ export default function  sectiontwo  () {
             <Image src={slash} alt='' width={200} height={200}/>
             <h1 className='font-semibold  text-3xl'>Feel the best experience with <br /> our features</h1>
           </div>
-          <div className='grid lg:grid-cols-3 grid-cols-2 mt-30 gap-20 justify-center'>
-            <div className=''>
-                <h1>sssssssssss</h1>
-                <p>ddddddddd</p>
+          <div className='grid lg:grid-cols-3 grid-cols-2 px-20 mt-30 gap-20 justify-center'>
+            <div className='flex flex-col text-center  justify-center items-center'>
+                <Image src={Invest1} alt='Image' width={200} height={200} className='w-10 h-10'/>
+                <h1 className='text-black'>Invest</h1>
+                <p className=' text-black text-[15px] w-60'>Invest your money on agriculture in Africa and get dividend after harvesting</p>
             </div>
             <div>dddddddddddddd</div>
             <div>dddddddddddddd</div>
